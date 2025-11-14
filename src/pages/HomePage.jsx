5eba1a884640cdf3b98844597fd7750b6d1bdd96
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-14 15:08:50 +0100

    add image in jumbo, not css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
 
 
     return (
         <>
-            <h1>ciao</h1>
-            <p>come va</p>
-            amicone    </>
+
+            <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">Qui trovi tutti i prodotti che desideri!</h1>
+                    <p class="col-md-8 fs-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem maxime ea debitis dignissimos, repudiandae nulla dicta, eius totam accusamus nemo assumenda, fuga ex deserunt facere optio. Reiciendis, quas cupiditate.
+                    </p>
+                    <button class="btn btn-primary btn-lg" type="button">
+                        <Link to="/products">Scopri i prodotti!</Link>
+                    </button>
+                </div>
+            </div>
+
+
+
+        </>
     )
 }
